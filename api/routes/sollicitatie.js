4f00eb65bbd5bb025d2159aa27683eb370eaaf90
Mauri <info@brandlift.nl>
2025-07-22 15:31:41 +0200

    solliciteren - debugging api - age

--- api/routes/sollicitatie.js
+++ api/routes/sollicitatie.js
@@ -118,7 +118,7 @@ export default async function handler(req, res) {
     // Valideer leeftijd (minimaal 18 jaar)
     const birthDate = new Date(geboortedatum);
     const today = new Date();
-    const age = today.getFullYear() - birthDate.getFullYear();
+    let age = today.getFullYear() - birthDate.getFullYear();
     const monthDiff = today.getMonth() - birthDate.getMonth();
     
     if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
