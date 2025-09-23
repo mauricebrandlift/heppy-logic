65b71f748a580d8c1f9b5dc08a08751727c8a6f2
Mauri <info@brandlift.nl>
2025-09-23 15:33:18 +0200

    abonnement - persoonsgegevens - init next step2

--- public/forms/aanvraag/abbOverzicht.js
+++ public/forms/aanvraag/abbOverzicht.js
@@ -101,7 +101,7 @@ export function initAbbOverzicht() {
         // Geen extra actie; alle data is al in de flow bewaard door eerdere stappen
       },
       onSuccess: () => {
-        // Volg hetzelfde patroon als andere stappen: eerst module laden + init, daarna navigerenn
+        // Volg hetzelfde patroon als andere stappen: eerst module laden + init, daarna navigeren
         import('./abbPersoonsgegevensForm.js')
           .then((m) => {
             if (m && typeof m.initAbbPersoonsgegevensForm === 'function') {
