9c3cd0fda82035b0cd73bab1582da6fd800d2e75
Mauri <info@brandlift.nl>
2025-11-07 10:14:36 +0100

    succes redirect fix

--- public/forms/tapijt/rtAdresForm.js
+++ public/forms/tapijt/rtAdresForm.js
@@ -5,7 +5,7 @@ import { formHandler } from '../logic/formHandler.js';
 import { getFormSchema } from '../schemas/formSchemas.js';
 import { fetchAddressDetails, fetchCoverageStatus, ApiError as FrontendApiError } from '../../utils/api/index.js';
 import { showError, hideError } from '../ui/formUi.js';
-import { saveFlowData, loadFlowData } from '../logic/formStorage.js';
+import { saveFlowData, loadFlowData, clearOtherFlows } from '../logic/formStorage.js';
 import { logStepCompleted } from '../../utils/tracking/simpleFunnelTracker.js';
 
 const FORM_NAME = 'rt_adres-form';
@@ -44,6 +44,9 @@ function goToFormStep(nextFormName) {
  */
 export function initRtAdresForm() {
   console.log('[rtAdresForm] Initialiseren van formulier:', FORM_NAME);
+  
+  // ✨ Clear andere flow data om vervuiling te voorkomen
+  clearOtherFlows('tapijt-aanvraag');
   
   // Haal het schema op
   const schema = getFormSchema(FORM_NAME);
